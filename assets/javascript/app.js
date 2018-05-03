036e7bc27b25335e51d6edd01e76ac6e3c9d5fc4
Tobey Ross <tobross@gmail.com>
2018-05-03 08:39:21 -0500

    connected links with firebase, javascript, and CSS, initial html structure, and reset.css

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyBeMkHv3bt0v0tXoc6UikMbHoZTtEMU-v0",
+    authDomain: "g3p1-66142.firebaseapp.com",
+    databaseURL: "https://g3p1-66142.firebaseio.com",
+    projectId: "g3p1-66142",
+    storageBucket: "g3p1-66142.appspot.com",
+    messagingSenderId: "127127300038"
+  };
+  firebase.initializeApp(config);
+ var database = firebase.database();
