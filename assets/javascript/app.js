1fcf47af1598d8ff0bafae4e138d0197b995321a
erikamats <erikamats@hotmail.com>
2018-05-04 13:35:38 -0500

    basic index

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,21 @@
- // Initialize Firebase
- var config = {
-    apiKey: "AIzaSyBeMkHv3bt0v0tXoc6UikMbHoZTtEMU-v0",
-    authDomain: "g3p1-66142.firebaseapp.com",
-    databaseURL: "https://g3p1-66142.firebaseio.com",
-    projectId: "g3p1-66142",
-    storageBucket: "g3p1-66142.appspot.com",
-    messagingSenderId: "127127300038"
-  };
-  firebase.initializeApp(config);
- var database = firebase.database();
+//  // Initialize Firebase
+//  var config = {
+//     apiKey: "AIzaSyBeMkHv3bt0v0tXoc6UikMbHoZTtEMU-v0",
+//     authDomain: "g3p1-66142.firebaseapp.com",
+//     databaseURL: "https://g3p1-66142.firebaseio.com",
+//     projectId: "g3p1-66142",
+//     storageBucket: "g3p1-66142.appspot.com",
+//     messagingSenderId: "127127300038"
+//   };
+//   firebase.initializeApp(config);
+
+//  var database = firebase.database();
+
+
+//  dropdown nav functionality
+$(document).ready(function(){
+  $(".dropdown-trigger").dropdown();
+  $('.parallax').parallax();
+});
+
+
